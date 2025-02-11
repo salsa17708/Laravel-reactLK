a5bdc1945a1c8422f01bcf1dc8144829d0a9b054
salsa17708 <salsabila17708@gmail.com>
2025-02-11 11:46:04 +0700

    mengisi postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,65 +1,88 @@
 <?php
-
+   
 namespace App\Http\Controllers;
-
-use App\Models\Post;
+    
 use Illuminate\Http\Request;
-
+use Inertia\Inertia;
+use App\Models\Post;
+use Illuminate\Support\Facades\Validator;
+   
 class PostController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Show the form for creating a new resource.
+     *
+     * @return Response
      */
-    public function index()
+    public function index() /*untuk menampilkan daftar post*/
     {
-        //
+        $posts = Post::all(); /*untuk mengambil semua data di database*/
+        return Inertia::render('Posts/Index', ['posts' => $posts]); /*untuk menampilkan tampilan dengan menggunakan framework Inertia*/
     }
-
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create() /*untuk menampilkan create*/
+    {
+        return Inertia::render('Posts/Create');
+    }
+    
     /**
      * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     *
+     * @return Response
      */
     public function store(Request $request)
     {
-        //
+        Validator::make($request->all(), [
+            'title' => ['required'],
+            'body' => ['required'],
+        ])->validate(); /*untuk memastikan data yang di terima*/
+   
+        Post::create($request->all());
+            
+        return redirect(route('posts.index'))->with('success', 'Post created.'); /*mengarahkan kembari ke halaman home dgn pesan sukses*/
     }
-
+  
     /**
-     * Display the specified resource.
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
+     * Write code on Method
+     *
+     * @return response()
      */
     public function edit(Post $post)
     {
-        //
+        return Inertia::render('Posts/Edit', [
+            'post' => $post
+        ]);
     }
-
+    
     /**
-     * Update the specified resource in storage.
+     * Show the form for creating a new resource.
+     *
+     * @return Response
      */
-    public function update(Request $request, Post $post)
+    public function update($id, Request $request)
     {
-        //
+        Validator::make($request->all(), [
+            'title' => ['required'],
+            'body' => ['required'],
+        ])->validate();
+    
+        Post::find($id)->update($request->all());
+        return redirect(route('posts.index'))->with('success', 'Post updated.');
     }
-
+    
     /**
-     * Remove the specified resource from storage.
+     * Show the form for creating a new resource.
+     *
+     * @return Response
      */
-    public function destroy(Post $post)
+    public function destroy($id)
     {
-        //
+        Post::find($id)->delete();
+        return redirect(route('posts.index'))->with('success', 'Post deleted.');
     }
-}
+}  
